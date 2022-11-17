1b771f6806280108198bfb9ea806689814e780dd
Javier Flores <javier.floresapodaca@gmail.com>
2022-11-16 22:52:19 -0800

    Worked on inferred typing with variables

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,4 +1,5 @@
-let apples: number = 5;
+let apples = 5;
+
 let speed: string = 'fast';
 let hasName: boolean = true;
 
@@ -27,3 +28,29 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+// console.log(coordinates.sdkfhkjdsfh);
+
+// 2) When we declare a variable on one line
+// and initiate it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
