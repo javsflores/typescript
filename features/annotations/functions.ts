2c5c4a676da2e8887e1167dd0437ddf469428be6
Javier Flores <javier.floresapodaca@gmail.com>
2022-11-16 22:52:46 -0800

    Worked on inferred typing with functions

---
+++ features/annotations/functions.ts
@@ -0,0 +1,35 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const substract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = (a: number, b: number): number => {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = (forecast: { date: Date; weather: string }): void => {
+  console.log(forecast.date);
+  console.log(forecast.weather);
+};
+
+logWeather(todaysWeather);
