063d7930e289a27296ad8017fd3d6a5468b50fc0
Javier Flores <javier.floresapodaca@selectquote.com>
2023-05-14 13:12:27 -0700

    TESTING

--- features/arrays.ts
+++ features/arrays.ts
@@ -5,7 +5,7 @@ const dates = [new Date(), new Date()];
 const carsByMake: string[][] = [['f150'], ['corolla'], ['camaro']];
 
 // Help with inference when extracting values
-car = carMakers[0];
+// car = carMakers[0]; // TESTING
 const myCar = carMakers.pop();
 
 // Prevent incompatible values
